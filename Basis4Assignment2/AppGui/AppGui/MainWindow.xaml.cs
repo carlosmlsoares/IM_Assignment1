37672541e4b062cd2b915c40cbf52b98354c3475
Rui-Jesus <r.jesus@ua.pt>
2019-11-13 17:16:42 +0000

    Gramatica e api done

--- Basis4Assignment2/AppGui/AppGui/MainWindow.xaml.cs
+++ Basis4Assignment2/AppGui/AppGui/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 using System.Windows.Input;
@@ -11,6 +12,8 @@ using Newtonsoft.Json;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
+using WindowsInput;
+using WindowsInput.Native;
 
 namespace AppGui
 {
@@ -21,15 +24,18 @@ namespace AppGui
     {
         private MmiCommunication mmiC;
         private IWebDriver driver;
+        private InputSimulator sim;
         public MainWindow()
         {
-            
+
             //InitializeComponent();
-            
+
 
             driver = new ChromeDriver(".");
+            System.Threading.Thread.Sleep(3000);
+            sim = new InputSimulator();
 
-            mmiC = new MmiCommunication("localhost",8000, "User1", "GUI");
+            mmiC = new MmiCommunication("localhost", 8000, "User1", "GUI");
             mmiC.Message += MmiC_Message;
             mmiC.Start();
 
@@ -42,17 +48,10 @@ namespace AppGui
             var com = doc.Descendants("command").FirstOrDefault().Value;
             dynamic json = JsonConvert.DeserializeObject(com);
 
-            Shape _s = null;
             switch ((string)json.recognized[0].ToString())
             {
-                case "SQUARE": _s = rectangle;
-                    break;
-                case "CIRCLE": _s = circle;
-                    break;
-                case "TRIANGLE": _s = triangle;
-                    break;
-                default:
-                    _s = triangle;
+
+                case "comando":
                     break;
             }
 
@@ -60,30 +59,98 @@ namespace AppGui
             {
                 switch ((string)json.recognized[1].ToString())
                 {
-                    case "GREEN":
-                        _s.Fill = Brushes.Green;
-                        break;
-                    case "BLUE":
-                        _s.Fill = Brushes.Blue;
-                        break;
-                    case "RED":
-                        _s.Fill = Brushes.Red;
-                        break;
-                    case "google":
-                        driver.Navigate().GoToUrl("https://www.google.com");
-                        Console.WriteLine("esta");
-                        break;
-                    case "aos favoritos":
-                        Actions builder = new Actions(driver);
 
-                        builder.KeyDown(Keys.Control).SendKeys(Key.D.ToString()).Perform();
+                    case "1": //bookmarking
+                        sim.Keyboard.KeyDown(VirtualKeyCode.CONTROL);
+                        sim.Keyboard.KeyPress(VirtualKeyCode.VK_D);
+                        sim.Keyboard.KeyUp(VirtualKeyCode.CONTROL);
+                        System.Threading.Thread.Sleep(100);
+                        sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+                        break;
 
+
+                    case "2": //maximize
+                        driver.Manage().Window.Maximize();
+                        break;
+                    case "3": //minimize
+                        driver.Manage().Window.Minimize();
+                        break;
+                    case "4": //new tab
+                        sim.Keyboard.KeyDown(VirtualKeyCode.CONTROL);
+                        sim.Keyboard.KeyPress(VirtualKeyCode.VK_T);
+                        sim.Keyboard.KeyUp(VirtualKeyCode.CONTROL);
+                        driver.SwitchTo().Window(driver.Url);
+
+                        break;
+                    case "5": //next tab
+                        sim.Keyboard.KeyDown(VirtualKeyCode.CONTROL);
+                        sim.Keyboard.KeyPress(VirtualKeyCode.TAB);
+                        sim.Keyboard.KeyUp(VirtualKeyCode.CONTROL);
+                        driver.SwitchTo().Window(driver.Url);
+
+                        break;
+                    case "6": //prev tab
+                        sim.Keyboard.KeyDown(VirtualKeyCode.CONTROL);
+                        sim.Keyboard.KeyDown(VirtualKeyCode.SHIFT);
+                        sim.Keyboard.KeyPress(VirtualKeyCode.TAB);
+                        sim.Keyboard.KeyUp(VirtualKeyCode.CONTROL);
+                        sim.Keyboard.KeyUp(VirtualKeyCode.SHIFT);
+                        driver.SwitchTo().Window(driver.Url);
+                        break;
+                    case "7": //open history
+                        sim.Keyboard.KeyDown(VirtualKeyCode.CONTROL);
+                        sim.Keyboard.KeyPress(VirtualKeyCode.VK_H);
+                        sim.Keyboard.KeyUp(VirtualKeyCode.CONTROL);
+                        driver.SwitchTo().Window(driver.Url);
+                        break;
+                    case "8": //close tab
+                        sim.Keyboard.KeyDown(VirtualKeyCode.CONTROL);
+                        sim.Keyboard.KeyPress(VirtualKeyCode.VK_W);
+                        sim.Keyboard.KeyUp(VirtualKeyCode.CONTROL);
+                        driver.SwitchTo().Window(driver.Url);
+                        break;
+                    case "9": //zoom in
+                        sim.Keyboard.KeyDown(VirtualKeyCode.CONTROL);
+                        sim.Keyboard.KeyPress(VirtualKeyCode.OEM_PLUS);
+                        sim.Keyboard.KeyUp(VirtualKeyCode.CONTROL);
+                        break;
+                    case "10": //zoom out
+                        sim.Keyboard.KeyDown(VirtualKeyCode.CONTROL);
+                        sim.Keyboard.KeyPress(VirtualKeyCode.OEM_MINUS);
+                        sim.Keyboard.KeyUp(VirtualKeyCode.CONTROL);
+                        break;
+                    case "11": //open google
+                        //driver.Url = "www.google.com";
+                        break;
+                    case "12": //open favorites
+                        sim.Keyboard.KeyDown(VirtualKeyCode.CONTROL);
+                        sim.Keyboard.KeyDown(VirtualKeyCode.SHIFT);
+                        sim.Keyboard.KeyPress(VirtualKeyCode.VK_O);
+                        sim.Keyboard.KeyUp(VirtualKeyCode.CONTROL);
+                        sim.Keyboard.KeyUp(VirtualKeyCode.SHIFT);
+
+                        break;
+
+                    case "13":
+                        ICollection<IWebElement> webElements = driver.FindElements(By.TagName("a"));
+                        foreach (IWebElement we in webElements)
+                        {
+                            ICollection<IWebElement> results = we.FindElements(By.TagName("h3"));
+                            if (results.Count != 0)
+                            {
+                                Console.WriteLine(results.First().Text.ToString() + "\t" + we.GetAttribute("href"));
+                            }
+
+                        }
+
+
+                        //results.First().Click();w
 
                         break;
 
                 }
             });
-            
+
 
 
         }
--- Basis4Assignment2/speechModality/speechModality/SpeechMod.cs
+++ Basis4Assignment2/speechModality/speechModality/SpeechMod.cs
@@ -5,12 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using mmisharp;
 using Microsoft.Speech.Recognition;
+using System.IO;
+using System.Globalization;
+using Microsoft.Speech.Recognition.SrgsGrammar;
 
 namespace speechModality
 {
     public class SpeechMod
     {
         private SpeechRecognitionEngine sre;
+        private GrammarBuilder builder;
         private Grammar gr;
         public event EventHandler<SpeechEventArg> Recognized;
         protected virtual void onRecognized(SpeechEventArg msg)
@@ -34,11 +38,29 @@ namespace speechModality
 
             mmic.Send(lce.NewContextRequest());
 
-            //load pt recognizer
-            sre = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("pt-PT"));
-            gr = new Grammar(Environment.CurrentDirectory + "\\ptG.grxml", "rootRule");
-            sre.LoadGrammar(gr);
+            var sampleDoc = new SrgsDocument(Environment.CurrentDirectory + "\\ptG.grxml");
+            sampleDoc.Culture = CultureInfo.GetCultureInfo("pt-PT");
 
+            /*SrgsRule searchWordsRule = new SrgsRule("SearchWords");
+            searchWordsRule.Add(new SrgsOneOf(                                          
+                new SrgsItem("footebol")
+            ));
+
+            sampleDoc.Rules.Add(searchWordsRule);*/
+            gr = new Grammar(sampleDoc);
+
+            //load pt recognizer
+            System.Globalization.CultureInfo culture = new System.Globalization.CultureInfo("pt-PT");
+            sre = new SpeechRecognitionEngine(culture);
+
+            /*
+            builder = new GrammarBuilder();
+            builder.Culture = culture;
+            builder.AppendRuleReference(Environment.CurrentDirectory + "\\ptG.grxml");
+            //gr = new Grammar(Environment.CurrentDirectory + "\\ptG.grxml", "rootRule");
+            gr = new Grammar(builder);
+            */
+            sre.LoadGrammar(gr);
             
             sre. SetInputToDefaultAudioDevice();
             sre.RecognizeAsync(RecognizeMode.Multiple);
@@ -54,19 +76,33 @@ namespace speechModality
 
         private void Sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
+
             onRecognized(new SpeechEventArg(){Text = e.Result.Text, Confidence = e.Result.Confidence, Final = true});
-            
+
             //SEND
             // IMPORTANT TO KEEP THE FORMAT {"recognized":["SHAPE","COLOR"]}
             string json = "{ \"recognized\": [";
             foreach (var resultSemantic in e.Result.Semantics)
             {
-                json+= "\"" + resultSemantic.Value.Value +"\", ";
+                Console.WriteLine(resultSemantic.Key.ToString());
+                if(resultSemantic.Key == "SearchObject")
+                {
+                    foreach (var val in resultSemantic.Value.ToArray())
+                    {
+                        json += "\"" + val.Value.Value + "\", ";
+                    }
+                }
+                else
+                {
+                    json += "\"" + resultSemantic.Value.Value + "\", ";
+                }
+                
             }
             json = json.Substring(0, json.Length - 2);
             json += "] }";
 
             var exNot = lce.ExtensionNotification(e.Result.Audio.StartTime+"", e.Result.Audio.StartTime.Add(e.Result.Audio.Duration)+"",e.Result.Confidence, json);
+            Console.WriteLine(exNot.ToString());
             mmic.Send(exNot);
         }
     }
